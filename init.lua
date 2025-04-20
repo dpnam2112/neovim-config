97a268bf0e3ac1841572a10a73c294f09bb407ce
Nam Phuong Do <dpnam2112@gmail.com>
2025-04-20 21:58:44 +0700

    chore: run formatter

--- init.lua
+++ init.lua
@@ -1,26 +1,26 @@
-vim.g.mapleader = " "  -- Set <leader> to Space
+vim.g.mapleader = " " -- Set <leader> to Space
 
 -- General settings
-vim.o.number = true	      -- Show line numbers
-vim.o.relativenumber = true	      -- Relative line numbers
-vim.o.linebreak = true	      -- Break lines at word (requires Wrap lines)
-vim.o.showbreak = '+++'	      -- Wrap-broken line prefix
-vim.o.textwidth = 100	      -- Line wrap (number of cols)
-vim.o.showmatch = true	      -- Highlight matching brace 
-vim.o.encoding = 'utf-8'
-vim.opt.cursorline = true 	-- Highlight line 
-vim.o.hlsearch = true         -- Highlight all search results
-vim.o.smartcase = true        -- Enable smart-case search
-vim.o.ignorecase = true       -- Always case-insensitive
-vim.o.incsearch = true        -- Searches for strings incrementally
+vim.o.number = true -- Show line numbers
+vim.o.relativenumber = true -- Relative line numbers
+vim.o.linebreak = true -- Break lines at word (requires Wrap lines)
+vim.o.showbreak = "+++" -- Wrap-broken line prefix
+vim.o.textwidth = 100 -- Line wrap (number of cols)
+vim.o.showmatch = true -- Highlight matching brace
+vim.o.encoding = "utf-8"
+vim.opt.cursorline = true -- Highlight line
+vim.o.hlsearch = true -- Highlight all search results
+vim.o.smartcase = true -- Enable smart-case search
+vim.o.ignorecase = true -- Always case-insensitive
+vim.o.incsearch = true -- Searches for strings incrementally
 
-vim.o.autoindent = true       -- Auto-indent new lines
-vim.o.smarttab = true         -- Enable smart-tabs
+vim.o.autoindent = true -- Auto-indent new lines
+vim.o.smarttab = true -- Enable smart-tabs
 
 -- Advanced settings
-vim.o.ruler = true            -- Show row and column ruler information
-vim.o.undolevels = 1000       -- Number of undo levels
-vim.o.backspace = 'indent,eol,start'  -- Backspace behavior
+vim.o.ruler = true -- Show row and column ruler information
+vim.o.undolevels = 1000 -- Number of undo levels
+vim.o.backspace = "indent,eol,start" -- Backspace behavior
 
 vim.opt.shiftwidth = 2
 
@@ -28,14 +28,14 @@ vim.opt.shiftwidth = 2
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -47,18 +47,18 @@ require("keymaps")
 -- statusline
 require("lualine").setup()
 require("config")
-require("config.nvim_cmp")  -- Ensure this line exists in your init.lua
+require("config.nvim_cmp") -- Ensure this line exists in your init.lua
 require("lsp_config")
 
 -- Set tabs for Go files
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "go",
-    callback = function()
-        vim.opt_local.expandtab = false  -- Use tabs, not spaces
-        vim.opt_local.tabstop = 4        -- Tab width = 4 spaces
-        vim.opt_local.shiftwidth = 4     -- Indentation level
-        vim.opt_local.softtabstop = 4    -- Makes Tab and Backspace work properly
-    end
+	pattern = "go",
+	callback = function()
+		vim.opt_local.expandtab = false -- Use tabs, not spaces
+		vim.opt_local.tabstop = 4 -- Tab width = 4 spaces
+		vim.opt_local.shiftwidth = 4 -- Indentation level
+		vim.opt_local.softtabstop = 4 -- Makes Tab and Backspace work properly
+	end,
 })
 
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,29 +5,34 @@
 -- <leader> is set in `init.lua`.
 
 -- Toggle file explorer
-vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Go to definition in a new tab
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"gd",
+	"<cmd>tab split | lua vim.lsp.buf.definition()<CR>",
+	{ noremap = true, silent = true }
+)
 
 -- Telescope keymaps
 local telescope_builtin = require("telescope.builtin")
 
 -- VSCode-like file finder
-vim.keymap.set('n', '<C-p>', telescope_builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<C-p>", telescope_builtin.find_files, { desc = "Find files" })
 
 -- Open buffer list
-vim.keymap.set('n', '<C-b>', telescope_builtin.buffers, { desc = "Open buffers (Telescope)" })
+vim.keymap.set("n", "<C-b>", telescope_builtin.buffers, { desc = "Open buffers (Telescope)" })
 
 -- Live grep across files
-vim.keymap.set('n', '<C-f>', telescope_builtin.live_grep, { desc = "Search in files (grep)" })
+vim.keymap.set("n", "<C-f>", telescope_builtin.live_grep, { desc = "Search in files (grep)" })
 
 -- Open command palette (like VSCode)
-vim.keymap.set('n', '<leader>:', telescope_builtin.commands, { desc = "Command palette" })
+vim.keymap.set("n", "<leader>:", telescope_builtin.commands, { desc = "Command palette" })
 
 -- Other useful Telescope bindings
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = "Find buffers" })
-vim.keymap.set('n', '<leader>fr', telescope_builtin.oldfiles, { desc = "Recent files" })
-vim.keymap.set('n', '<leader>fg', telescope_builtin.git_files, { desc = "Git files" })
-vim.keymap.set('n', '<leader>ss', telescope_builtin.lsp_document_symbols, { desc = "Document symbols" })
-vim.keymap.set('n', '<leader>xx', telescope_builtin.diagnostics, { desc = "Diagnostics" })
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fr", telescope_builtin.oldfiles, { desc = "Recent files" })
+vim.keymap.set("n", "<leader>fg", telescope_builtin.git_files, { desc = "Git files" })
+vim.keymap.set("n", "<leader>ss", telescope_builtin.lsp_document_symbols, { desc = "Document symbols" })
+vim.keymap.set("n", "<leader>xx", telescope_builtin.diagnostics, { desc = "Diagnostics" })
