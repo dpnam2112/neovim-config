0fc9dc5f42a2d79514bea044c2079577894ea94d
Nam <dpnam2112@gmail.com>
2025-10-04 18:47:35 +0700

    Add config for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,23 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional but pretty
+  config = function()
+    require("lualine").setup {
+      options = {
+        theme = "catppuccin", -- auto picks your Catppuccin palette
+        icons_enabled = true,
+        section_separators = { left = "", right = "" },
+        component_separators = "|",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } }, -- path=1 shows relative path
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    }
+  end,
 }
+
