7bfe9ca62587ab6686f64c8b41f39ce2ce116fdb
Nam <dpnam2112@gmail.com>
2025-10-04 18:27:14 +0700

    Add style for catppuccin

--- lua/plugins/catppuccin.nvim.lua
+++ lua/plugins/catppuccin.nvim.lua
@@ -1,8 +1,47 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
-    priority = 1000
-  },
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = {      -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
+      show_end_of_buffer = false,
+      term_colors = true,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = { "bold" },
+        keywords = { "italic" },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = { "bold" },
+        properties = {},
+        types = { "italic", "bold" },
+        operators = {},
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        treesitter = true,
+        notify = true,
+        mini = true,
+      },
+    })
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
+
