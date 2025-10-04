ecba5bc95b0fe7983eb2e7ab999e4e4febbe6e17
Nam <dpnam2112@gmail.com>
2025-10-04 23:59:39 +0700

    chore: remove redundant config lines

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,4 @@
 return {
-  { "neovim/nvim-lspconfig" }, -- Provide default configurations for LSPs
-
-  { "hrsh7th/cmp-nvim-lsp" },
   { "L3MON4D3/LuaSnip" },
   { "junegunn/fzf.vim", dependencies = { "junegunn/fzf" } },
 }
